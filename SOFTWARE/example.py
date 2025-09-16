2cc28da19a030e4816244182664b75b0f41d9e7d
mayermakes <49558118+mayermakes@users.noreply.github.com>
2025-09-16 15:53:26 +0200

    update
    breaking changes

--- SOFTWARE/example.py
+++ SOFTWARE/example.py
@@ -1,13 +1,41 @@
 from esp32_usb_lib import ESP32USB
+import time
 
 esp = ESP32USB(port="/dev/ttyACM0")
+while True:
+	esp.setpin(2,0)
+	time.sleep(3)
+	esp.setpin(2, 1)  
+	        
+	val = esp.readpin(2)
+	print("digital value: "+ str(val))      
+	time.sleep(2)
+	print("set PWM to 40")
+	esp.setpwm(2,40) 
+	time.sleep(2)
+	print("set PWM to 80")
+	esp.setpwm(2, 80) 
+	time.sleep(2)
+	print("set PWM to 160")
+	esp.setpwm(2, 160) 
+	time.sleep(2)
+	print("set PWM to 255")
+	esp.setpwm(2, 255) 
+	time.sleep(3)  
+	adc = esp.analogread(2)
+	print("analog value: "+ str(adc))     
+	time.sleep(1)
+	esp.setpwm(2, 100)
+	time.sleep(3)
+	byte=0x55
+	esp.i2c_write(0x3C, byte) 
+	print("Byte written: " + str(byte))
+	data = esp.i2c_read(0x3C, 4)
+	print("Byte read back: " + str(data))
+	time.sleep(2)
+	
 
-esp.setpin(5, 1)          # Set GPIO 5 HIGH
-val = esp.readpin(5)      # Read GPIO 5 digital
-adc = esp.analogread(4)   # Read analog from GPIO 4
-esp.setpwm(6, 128)        # PWM 50% duty on GPIO 6
-esp.i2c_write(0x3C, 0x55) # Write byte
-data = esp.i2c_read(0x3C, 4)
+
 
 esp.close()
 
